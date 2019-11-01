8f5e743e47338d9d53338b6e08e5080cff873fef
Gabriel Cardona <cgcardona@gmail.com>
2019-11-01 15:24:09 -0700

    Serialize Block, BlockHeader and Vec<Transction> structs

--- blockchain/src/block.rs
+++ blockchain/src/block.rs
@@ -1,6 +1,8 @@
+use crate::{BlockHeader, Transaction};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Block {
-    pub transactions: Vec<String>,
+    pub block_header: BlockHeader,
+    pub transactions: Vec<Transaction>,
 }
--- blockchain/src/lib.rs
+++ blockchain/src/lib.rs
@@ -1,3 +1,7 @@
 mod block;
+mod block_header;
+mod transaction;
 
 pub use block::Block;
+pub use block_header::BlockHeader;
+pub use transaction::Transaction;
