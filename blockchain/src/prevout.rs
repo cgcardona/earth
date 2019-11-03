022b442020cf202816a1db9a506354c17adf900b
Gabriel Cardona <cgcardona@gmail.com>
2019-11-03 11:29:55 -0800

    Default and From for PrevOut.

--- blockchain/src/prevout.rs
+++ blockchain/src/prevout.rs
@@ -3,14 +3,20 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PrevOut {}
 
-impl From<&'static str> for PrevOut {
-    fn from(s: &'static str) -> Self {
-        PrevOut::new()
+impl PrevOut {
+    pub fn new() -> Self {
+        Default::default()
     }
 }
 
-impl PrevOut {
-    pub fn new() -> Self {
+impl Default for PrevOut {
+    fn default() -> Self {
         PrevOut {}
+    }
+}
+
+impl From<&'static str> for PrevOut {
+    fn from(s: &'static str) -> Self {
+        PrevOut::new()
     }
 }
