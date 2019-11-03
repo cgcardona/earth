52e9d256a95708ac1eb60dfa27346fd7ab21faf2
Gabriel Cardona <cgcardona@gmail.com>
2019-11-03 11:34:01 -0800

    Default and From impl for TxOutput.

--- blockchain/src/txoutput.rs
+++ blockchain/src/txoutput.rs
@@ -6,17 +6,40 @@ pub struct TxOutput {
     pub script_pubkey: String,
 }
 
-impl From<&'static str> for TxOutput {
-    fn from(s: &'static str) -> Self {
-        TxOutput::new(1, String::from(s))
+impl TxOutput {
+    pub fn new(value: Option<u64>, script_pubkey: Option<String>) -> Self {
+        match (value, script_pubkey) {
+            (Some(v), Some(s)) => TxOutput {
+                value: v,
+                script_pubkey: s,
+            },
+            (Some(v), None) => TxOutput {
+                value: v,
+                script_pubkey: Default::default(),
+            },
+            (None, Some(s)) => TxOutput {
+                value: Default::default(),
+                script_pubkey: s,
+            },
+            (None, None) => TxOutput {
+                value: Default::default(),
+                script_pubkey: Default::default(),
+            },
+        }
     }
 }
 
-impl TxOutput {
-    pub fn new(value: u64, script_pubkey: String) -> Self {
+impl Default for TxOutput {
+    fn default() -> Self {
         TxOutput {
-            value: value,
-            script_pubkey: script_pubkey,
+            value: Default::default(),
+            script_pubkey: Default::default(),
         }
+    }
+}
+
+impl From<&'static str> for TxOutput {
+    fn from(s: &'static str) -> Self {
+        TxOutput::new(Some(1), Some(String::from(s)))
     }
 }
