dce69fb68b5fdda4384b29bad8daf055c6dfab39
Gabriel Cardona <cgcardona@gmail.com>
2019-11-03 11:17:22 -0800

    impl new and default for Block.

--- blockchain/src/block.rs
+++ blockchain/src/block.rs
@@ -7,18 +7,57 @@ pub struct Block {
     pub transactions: Vec<Transaction>,
 }
 
-impl From<&'static str> for Block {
-    fn from(s: &'static str) -> Self {
-        deserialize(s)
+impl Block {
+    pub fn new(header: Option<Header>, transactions: Option<Vec<Transaction>>) -> Self {
+        match (header, transactions) {
+            (Some(h), Some(t)) => Block {
+                header: h,
+                transactions: t,
+            },
+            (Some(h), None) => Block {
+                header: h,
+                transactions: Default::default(),
+            },
+            (None, Some(t)) => Block {
+                header: Default::default(),
+                transactions: t,
+            },
+            (None, None) => Default::default(),
+        }
     }
 }
 
-impl Block {
-    pub fn new(header: Header, transactions: Vec<Transaction>) -> Self {
+impl Default for Block {
+    fn default() -> Self {
         Block {
-            header: header,
-            transactions: transactions,
+            header: Header {
+                version: 1,
+                time: 1,
+                bits: 1,
+                nonce: 1,
+                prev_hash: String::from(""),
+                merkle_hash: String::from(""),
+            },
+            transactions: vec![Transaction {
+                version: 1,
+                lock_time: 1,
+                inputs: vec![TxInput {
+                    sequence: 1,
+                    script_sig: String::from(""),
+                    prev_out: PrevOut {},
+                }],
+                outputs: vec![TxOutput {
+                    value: 1,
+                    script_pubkey: String::from(""),
+                }],
+            }],
         }
+    }
+}
+
+impl From<&'static str> for Block {
+    fn from(s: &'static str) -> Self {
+        deserialize(s)
     }
 }
 
